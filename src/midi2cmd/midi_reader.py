3c9b8269cf2d5d5dcadae86e0e4a69d34a3da843
Jordi Funollet <funollet@fastmail.fm>
2024-12-10 21:47:50 +0100

    feat: Refactor MIDI message reading and add message processing function

--- src/midi2cmd/midi_reader.py
+++ src/midi2cmd/midi_reader.py
@@ -1,27 +1,28 @@
-import mido
+from mido import open_input
 
 
 def read_midi_messages(port_name):
     """Read and yield MIDI messages from the specified input port."""
-    with mido.open_input(port_name) as inport:
+    with open_input(port_name) as inport:
         for message in inport:
             yield message
 
 
-def main():
-    # List available input ports
-    print("Available MIDI input ports:")
-    input_ports = mido.get_input_names()
-    for port in input_ports:
-        print(port)
-
-    if not input_ports:
-        print("No MIDI input ports available.")
-    # Use the first available input port
-    port_name = input_ports[0]
-    for message in read_midi_messages(port_name):
-        print(message)
-
-
-if __name__ == "__main__":
-    main()
+def process_message(message):
+    cmds = {
+        (
+            10,
+            "control_change",
+            9,
+        ): "pactl set-sink-volume @DEFAULT_SINK@ $((MIDI_VALUE * 512))",
+        (10, "control_change", 25): "xdotool key ctrl+shift+h",  # raise hand in Meet
+        (10, "pitch"): "echo pitch changed",
+    }
+    cmd = cmds.get((message.type, message.control)) or cmds.get(
+        (
+            message.type,
+            message.control,
+            message.value,
+        )
+    )
+    print(cmd)
