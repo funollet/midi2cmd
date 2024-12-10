69eabe4f507aaab143e0e2484f8c8f94ec918cfd
Jordi Funollet <funollet@fastmail.fm>
2024-12-10 21:47:50 +0100

    refactor: Replace tuple keys with namedtuple for better readability in cmds

--- src/midi2cmd/midi_reader.py
+++ src/midi2cmd/midi_reader.py
@@ -1,4 +1,5 @@
 from mido import open_input
+from collections import namedtuple
 
 
 def read_midi_messages(port_name):
@@ -9,20 +10,14 @@ def read_midi_messages(port_name):
 
 
 def process_message(message):
+    CommandKey = namedtuple('CommandKey', ['channel', 'type', 'control'])
+    
     cmds = {
-        (
-            10,
-            "control_change",
-            9,
-        ): "pactl set-sink-volume @DEFAULT_SINK@ $((MIDI_VALUE * 512))",
-        (10, "control_change", 25): "xdotool key ctrl+shift+h",  # raise hand in Meet
-        (10, "pitch"): "echo pitch changed",
+        CommandKey(10, "control_change", 9): "pactl set-sink-volume @DEFAULT_SINK@ $((MIDI_VALUE * 512))",
+        CommandKey(10, "control_change", 25): "xdotool key ctrl+shift+h",  # raise hand in Meet
+        CommandKey(10, "pitch", None): "echo pitch changed",
     }
-    cmd = cmds.get((message.type, message.control)) or cmds.get(
-        (
-            message.type,
-            message.control,
-            message.value,
-        )
-    )
+    
+    key = CommandKey(message.type, message.control, getattr(message, 'value', None))
+    cmd = cmds.get(key)
     print(cmd)
