9ff192f3abab14a26a14cd55b21844a42c895dcc
Jordi Funollet <funollet@fastmail.fm>
2024-12-10 21:47:50 +0100

    option --port can be -p

--- src/midi2cmd/console.py
+++ src/midi2cmd/console.py
@@ -1,24 +1,32 @@
 import click
-import mido
-from midi_reader import read_midi_messages
+from midi_reader import process_message, read_midi_messages
+from mido import get_input_names
 
 
 @click.command(context_settings=dict(ignore_unknown_options=True))
-@click.option('--port', 'port_name', required=False, help='Name of the MIDI input port to use.')
+@click.option(
+    "-p",
+    "--port",
+    "port_name",
+    required=False,
+    help="Name of the MIDI input port to use.",
+)
 def main(port_name):
     """Read and print MIDI messages from the specified input port."""
     if not port_name:
         click.echo(main.get_help(click.Context(main)))
         click.Context(main).exit()
     try:
-        available_ports = mido.get_input_names()
+        available_ports = get_input_names()
         if port_name not in available_ports:
-            raise click.BadParameter(f"Port '{port_name}' is not available. Available ports: {', '.join(available_ports)}")
+            raise click.BadParameter(
+                f"Port '{port_name}' is not available. Available ports: {', '.join(available_ports)}"
+            )
     except Exception as e:
         raise click.ClickException(f"Error checking MIDI ports: {e}")
 
     for message in read_midi_messages(port_name):
-        print(message)
+        process_message(message)
 
 
 if __name__ == "__main__":
