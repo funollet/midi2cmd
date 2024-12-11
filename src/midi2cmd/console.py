e1336080bad4d4c8e4cc95b770d667bd420daeb8
Jordi Funollet <funollet@fastmail.fm>
2024-12-11 13:06:03 +0100

    refactor: --port is common to all commands

--- src/midi2cmd/console.py
+++ src/midi2cmd/console.py
@@ -6,6 +6,33 @@ from mido import get_input_names, open_input
 from midi2cmd.midi_reader import CommandBindings, process_message
 
 
+def validate_midi_port(port):
+    """Ensure a MIDI port can be opened."""
+    if port is None:
+        raise click.BadParameter(
+            f"Port '{port}' is not available. Hint: use `midi2cmd list`."
+        )
+    try:
+        with open_input(port):
+            pass
+    except OSError:
+        raise click.BadParameter(
+            f"Port '{port}' is not available. Hint: use `midi2cmd list`."
+        )
+
+
+def load_config_yaml(fname: str) -> str:
+    """Return the contents of the config file."""
+    cfg_yaml = ""
+    try:
+        with Path(fname).expanduser().open("r") as file:
+            cfg_yaml = file.read()
+    except FileNotFoundError:
+        raise click.BadParameter(f"Can't read file {fname}.")
+
+    return cfg_yaml
+
+
 @click.group()
 @click.option(
     "-c",
@@ -15,53 +42,42 @@ from midi2cmd.midi_reader import CommandBindings, process_message
     default="~/.config/midi2cmd/config.yaml",
     help="Configuration file.",
 )
-@click.pass_context
-def cli(ctx, config_path):
-    """MIDI command line interface."""
-    ctx.ensure_object(dict)
-    ctx.obj["config_path"] = config_path
-
-    # Load config file.
-    cfg_yaml = ""
-    try:
-        with Path(config_path).expanduser().open("r") as file:
-            cfg_yaml = file.read()
-    except FileNotFoundError:
-        raise click.BadParameter(f"Can't read file {config_path}.")
-    # Pass contents of config file as 'config'.
-    ctx.obj["config"] = cfg_yaml
-
-
-@cli.command()
-@click.pass_context
-def list(ctx):
-    """List available MIDI input ports."""
-    available_ports = get_input_names()
-    click.echo("Available MIDI input ports:")
-    for port in available_ports:
-        click.echo(f"- {port}")
-
-
-@cli.command()
 @click.option(
     "-p",
     "--port",
     "port_name",
     help="Name of the MIDI input port to use.",
-    required=True,
 )
 @click.pass_context
-def dump(ctx, port_name):
-    """Print MIDI messages as they are received."""
-    # config = ctx.obj["config"]
+def cli(ctx, config_path, port_name):
+    """MIDI command line interface."""
 
-    try:
-        with open_input(port_name) as inport:
-            pass
-    except OSError:
-        raise click.BadParameter(
-            f"Port '{port_name}' is not available. Hint: use `midi2cmd list`."
-        )
+    # ctx.obj is used to store shared state or defaults between commands.
+    ctx.ensure_object(dict)
+
+    cfg_yaml = load_config_yaml(config_path)
+
+    # Preserve parameters.
+    ctx.obj["config"] = cfg_yaml
+    ctx.obj["port_name"] = port_name
+
+
+@cli.command()
+def list():
+    """List available MIDI input ports."""
+    available_ports = get_input_names()
+    click.echo("Available MIDI input ports:")
+    for port in available_ports:
+        click.echo(f"    {port}")
+
+
+@cli.command()
+@click.pass_context
+def dump(ctx):
+    """Print MIDI messages as they are received."""
+    port_name = ctx.obj["port_name"]
+
+    validate_midi_port(port_name)
 
     with open_input(port_name) as inport:
         for message in inport:
@@ -69,25 +85,12 @@ def dump(ctx, port_name):
 
 
 @cli.command()
-@click.option(
-    "-p",
-    "--port",
-    "port_name",
-    help="Name of the MIDI input port to use.",
-    required=True,
-)
 @click.pass_context
-def run(ctx, port_name):
+def run(ctx):
     """Run the MIDI command processor."""
-    config = ctx.obj["config"]
+    config, port_name = ctx.obj["config"], ctx.obj["port_name"]
 
-    try:
-        with open_input(port_name) as inport:
-            pass
-    except OSError:
-        raise click.BadParameter(
-            f"Port '{port_name}' is not available. Hint: use `midi2cmd list`."
-        )
+    validate_midi_port(port_name)
 
     cmd_bindings = CommandBindings()
     cmd_bindings.from_yaml(config)
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,21 +1,52 @@
-from unittest.mock import patch
+from unittest.mock import mock_open, patch
 
+import click
 from click.testing import CliRunner
+from pytest import raises
 
-from midi2cmd.console import cli
+from midi2cmd.console import cli, load_config_yaml, validate_midi_port
+
+
+def test_load_config_yaml_success():
+    with patch("pathlib.Path.open", mock_open(read_data="is_yaml: true")):
+        result = load_config_yaml("dummy_path")
+        assert result == "is_yaml: true"
+
+
+def test_load_config_yaml_file_not_found():
+    with raises(click.BadParameter, match="Can't read file non_existent_file."):
+        load_config_yaml("non_existent_file")
+
+
+def test_validate_midi_port_valid():
+    with patch("midi2cmd.console.open_input") as mock_open_input:
+        mock_open_input.return_value.__enter__.return_value = None
+        validate_midi_port("ValidPort")
+        mock_open_input.assert_called_once_with("ValidPort")
+
+
+def test_validate_midi_port_invalid():
+    with raises(click.BadParameter):
+        with patch("midi2cmd.console.open_input", side_effect=OSError):
+            validate_midi_port("InvalidPort")
+
+
+def test_validate_midi_port_none():
+    with raises(click.BadParameter):
+        validate_midi_port(None)
 
 
 def test_cli_no_port():
     runner = CliRunner()
     result = runner.invoke(cli, ["run"])
     assert result.exit_code != 0
-    assert "Error: Missing option '-p' / '--port'." in result.output
+    assert "is not available" in result.output
 
 
 def test_cli_invalid_port():
     runner = CliRunner()
     with patch("midi2cmd.console.get_input_names", return_value=["ValidPort"]):
-        result = runner.invoke(cli, ["run", "--port", "InvalidPort"])
+        result = runner.invoke(cli, ["--port", "InvalidPort", "run"])
         assert result.exit_code != 0
         assert "Port 'InvalidPort' is not available." in result.output
 
@@ -25,7 +56,6 @@ def test_cli_help_option():
     result = runner.invoke(cli, ["--help"])
     assert result.exit_code == 0
     assert "Usage:" in result.output
-    assert "Print MIDI messages as they are received." in result.output
 
 
 def test_cli_list_ports():
@@ -34,5 +64,5 @@ def test_cli_list_ports():
         result = runner.invoke(cli, ["list"])
         assert result.exit_code == 0
         assert "Available MIDI input ports:" in result.output
-        assert "- Port1" in result.output
-        assert "- Port2" in result.output
+        assert " Port1" in result.output
+        assert " Port2" in result.output
