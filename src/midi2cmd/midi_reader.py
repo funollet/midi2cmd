26477bfebf2fc027577d788aff9db84db91e6e0b
Jordi Funollet <funollet@fastmail.fm>
2025-06-27 12:59:41 +0200

    chore: cleanup

--- src/midi2cmd/midi_reader.py
+++ src/midi2cmd/midi_reader.py
@@ -1,36 +1,5 @@
-from collections import namedtuple
-
 import mido
 from mido.frozen import freeze_message
-
-CommandKey = namedtuple("CommandKey", ["channel", "type", "control"])
-
-
-class CommandBindings(dict):
-    # Usage:
-    #
-    # c = CommandBindings()
-    # c[CommandKey(channel=10, type="control_change", control=18)] = "echo foo"
-
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    def load(self, channels):
-        """Transforms a dict of channels to CommandBindings"""
-        for channel, types in channels.items():
-            if "pitch" in types:
-                command = types["pitch"]
-                key = CommandKey(channel, "pitchwheel", "")
-                self.update({key: command})
-            if "control" in types:
-                for control, command in types["control"].items():
-                    key = CommandKey(channel, "control_change", control)
-                    self.update({key: command})
-
-    def for_message(self, msg):
-        """Returns the command associated to a given message, or ''"""
-        key = CommandKey(str(msg.channel), msg.type, str(getattr(msg, "control", "")))
-        return self.get(key)
 
 
 class MessageDict(dict):
