b32556df13ad336f104f5b6c08859cf90600160c
Jordi Funollet <funollet@fastmail.fm>
2025-06-27 13:07:02 +0200

    Mido message dict (#5)
    * refactor: MessageDict() is a specialized dict to map for miro.Message
    
    Uses miro.Message() as keys. Intended use: map shell commands with a
    "category" of MIDI messages (everything minus the value/pitch and time).

--- src/midi2cmd/midi_reader.py
+++ src/midi2cmd/midi_reader.py
@@ -1,30 +1,59 @@
-from collections import namedtuple
+import mido
+from mido.frozen import freeze_message  # type: ignore[import-untyped]
 
-CommandKey = namedtuple("CommandKey", ["channel", "type", "control"])
 
+class MessageDict(dict):
+    """
+    A dict with mido.Message's as keys.
+    Two messages are considered equal ignoring fields 'time', 'value' (for control_change events),
+    and 'pitch' (for pitchwheel events).
 
-class CommandBindings(dict):
-    # Usage:
-    #
-    # c = CommandBindings()
-    # c[CommandKey(channel=10, type="control_change", control=18)] = "echo foo"
+    Usage:
+
+        msg1 = mido.Message("control_change", channel=1, control=2)
+        msg2 = mido.Message("control_change", channel=1, control=2, value=10)
+
+        mc = MessageDict()
+        mc[msg1] = "echo foo"
+        mc[msg2]                   # returns "echo foo"
+    """
 
     def __init__(self, *args):
         super().__init__(*args)
 
-    def load(self, channels):
-        """Transforms a dict of channels to CommandBindings"""
-        for channel, types in channels.items():
-            if "pitch" in types:
-                command = types["pitch"]
-                key = CommandKey(channel, "pitchwheel", "")
-                self.update({key: command})
-            if "control" in types:
-                for control, command in types["control"].items():
-                    key = CommandKey(channel, "control_change", control)
-                    self.update({key: command})
+    def __setitem__(self, message, command):
+        """Sets a message and its associated command."""
+        # Default values for time, value, and pitch.
+        message.time = 0
+        if message.type == "pitchwheel":
+            message.pitch = 0
+        else:
+            message.value = 0
+        # Use freeze_message to ensure the message is hashable and can be used as a key.
+        super().__setitem__(freeze_message(message), command)
 
-    def for_message(self, msg):
-        """Returns the command associated to a given message, or ''"""
-        key = CommandKey(str(msg.channel), msg.type, str(getattr(msg, "control", "")))
-        return self.get(key)
+    def __getitem__(self, message):
+        """Returns the command associated to a given message, or ''."""
+        # Normalize the message before comparing
+        message.time = 0
+        if message.type == "pitchwheel":
+            message.pitch = 0
+        else:
+            message.value = 0
+
+        frozen_msg = freeze_message(message)
+        return self.get(frozen_msg, "")
+
+    def load(self, channels):
+        """Transforms a dict of channels to MessageDict."""
+        for channel, types in channels.items():
+            if "pitchwheel" in types:
+                command = types["pitchwheel"]
+                msg = mido.Message("pitchwheel", channel=int(channel))
+                self[msg] = command
+            if "control_change" in types:
+                for control, command in types["control_change"].items():
+                    msg = mido.Message(
+                        "control_change", channel=int(channel), control=int(control)
+                    )
+                    self[msg] = command
--- tests/test_midi_reader.py
+++ tests/test_midi_reader.py
@@ -1,58 +1,106 @@
-import tomllib
+import mido
 
-import pytest
-
-from midi2cmd.midi_reader import CommandBindings, CommandKey
+from midi2cmd.midi_reader import MessageDict
 
 
-@pytest.fixture
-def cmds():
-    """Fixture to parse command keys from TOML."""
-    txt = """
-        [channels.10]
-        pitch = "echo $MIDI_VALUE"
-        control.9 = "pactl set-sink-volume @DEFAULT_SINK@ $((MIDI_VALUE * 512))"  # control volume
-        control.18 = "[ $MIDI_VALUE = 0 ] && xdotool key ctrl+shift+h"            # raise hand in Meet
-    """
-    cfg = tomllib.loads(txt)
-    c = CommandBindings()
-    c.load(cfg["channels"])
-    return c
+def test_messagedict_control_change_set_and_get():
+    msg = mido.Message("control_change", channel=10, control=18, value=64)
+
+    mc = MessageDict()
+    mc[msg] = "echo foo"
+    assert mc[msg] == "echo foo"
 
 
-def test_config_reads_keys(cmds):
-    assert CommandKey("10", "pitchwheel", "") in cmds
-    assert CommandKey("10", "control_change", "9") in cmds
-    assert CommandKey("10", "control_change", "18") in cmds
+def test_messagedict_control_change_get_equivalent_items():
+    msg1 = mido.Message("control_change", channel=10, control=18, value=64)
+    msg2 = mido.Message("control_change", channel=10, control=18, value=33)
+
+    mc = MessageDict()
+    mc[msg1] = "echo foo"
+    mc[msg2] = "echo bar"
+    assert mc[msg1] == "echo bar"
+    assert mc[msg2] == "echo bar"
 
 
-def test_config_no_extra_keys(cmds):
-    assert len(cmds) == 3
+def test_messagedict_control_change_time_ignored():
+    msg1 = mido.Message("control_change", channel=10, control=18, value=64, time=100)
+    msg2 = mido.Message("control_change", channel=10, control=18, value=64, time=200)
+
+    mc = MessageDict()
+    mc[msg1] = "echo foo"
+    mc[msg2] = "echo bar"
+    assert mc[msg1] == "echo bar"
+    assert mc[msg2] == "echo bar"
 
 
-def test_config_reads_commands(cmds):
-    assert cmds[CommandKey("10", "pitchwheel", "")] == "echo $MIDI_VALUE"
-    assert (
-        cmds[CommandKey("10", "control_change", "9")]
-        == "pactl set-sink-volume @DEFAULT_SINK@ $((MIDI_VALUE * 512))"
-    )
-    assert (
-        cmds[CommandKey("10", "control_change", "18")]
-        == "[ $MIDI_VALUE = 0 ] && xdotool key ctrl+shift+h"
-    )
+def test_messagedict_pitchwheel_set_and_get():
+    msg = mido.Message("pitchwheel", channel=10, pitch=200)
+
+    mc = MessageDict()
+    mc[msg] = "echo foo"
+    assert mc[msg] == "echo foo"
 
 
-def test_commands_from_iterable():
-    c = CommandBindings()
-    c[CommandKey(10, "control_change", 18)] = (
-        "[ $MIDI_VALUE = 0 ] && xdotool key ctrl+shift+h"
-    )
-    assert (
-        c[CommandKey(10, "control_change", 18)]
-        == "[ $MIDI_VALUE = 0 ] && xdotool key ctrl+shift+h"
-    )
+def test_messagedict_pitchwheel_get_equivalent_items():
+    msg1 = mido.Message("pitchwheel", channel=10, pitch=200)
+    msg2 = mido.Message("pitchwheel", channel=10, pitch=1000)
+
+    mc = MessageDict()
+    mc[msg1] = "echo foo"
+    mc[msg2] = "echo bar"
+    assert mc[msg1] == "echo bar"
+    assert mc[msg2] == "echo bar"
 
 
-def test_commands_init():
-    c = CommandBindings([(CommandKey(10, "control_change", 18), "echo foo")])
-    assert c[CommandKey(10, "control_change", 18)] == "echo foo"
+def test_messagedict_pitchwheel_time_ignored():
+    msg1 = mido.Message("pitchwheel", channel=10, pitch=200, time=100)
+    msg2 = mido.Message("pitchwheel", channel=10, pitch=200, time=200)
+
+    mc = MessageDict()
+    mc[msg1] = "echo foo"
+    mc[msg2] = "echo bar"
+    assert mc[msg1] == "echo bar"
+    assert mc[msg2] == "echo bar"
+
+
+def test_messagedict_no_command():
+    mc = MessageDict()
+    msg = mido.Message("control_change", channel=10, control=18, value=64)
+    assert mc[msg] == ""
+
+
+def test_messagedict_multiple_messages():
+    mc = MessageDict()
+    msg1 = mido.Message("control_change", channel=1, control=2, value=10)
+    msg2 = mido.Message("pitchwheel", channel=1, pitch=200)
+    mc[msg1] = "cmd1"
+    mc[msg2] = "cmd2"
+    assert mc[msg1] == "cmd1"
+    assert mc[msg2] == "cmd2"
+
+
+def test_messagedict_load():
+    channels = {
+        "10": {
+            "pitchwheel": "echo pitch",
+            "control_change": {"9": "echo control9", "18": "echo control18"},
+        },
+        "2": {"control_change": {"1": "echo c2-1"}},
+    }
+    mc = MessageDict()
+    mc.load(channels)
+    # Check pitchwheel
+    msg_pitch = mido.Message("pitchwheel", channel=10)
+    assert mc[msg_pitch] == "echo pitch"
+    # Check control_change 9
+    msg_c9 = mido.Message("control_change", channel=10, control=9)
+    assert mc[msg_c9] == "echo control9"
+    # Check control_change 18
+    msg_c18 = mido.Message("control_change", channel=10, control=18)
+    assert mc[msg_c18] == "echo control18"
+    # Check control_change 2-1
+    msg_c2_1 = mido.Message("control_change", channel=2, control=1)
+    assert mc[msg_c2_1] == "echo c2-1"
+    # Check missing
+    msg_missing = mido.Message("control_change", channel=3, control=1)
+    assert mc[msg_missing] == ""
